53de7b9601190afcd623eaa4623b9e2fc780757b
Noah <nayagam1989@gmail.com>
2015-10-07 22:19:15 +0800

    #1 Initial commit

---
+++ WorshipSongs/WorshipSongs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorshipSongs
+//
+//  Created by Noah Nayagam on 10/7/15.
+//  Copyright © 2015 com.mcruncher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
