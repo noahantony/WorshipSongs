a58e609a8e7de0891d7a328e8a7ed937f93489ee
Noah <nayagam1989@gmail.com>
2015-10-14 06:52:55 +0800

    #4 Added the support to search in songs

--- WorshipSongs/WorshipSongs/SongsTableViewController.swift
+++ WorshipSongs/WorshipSongs/SongsTableViewController.swift
@@ -8,27 +8,50 @@
 
 import UIKit
 
-class SongsTableViewController: UITableViewController {
+class SongsTableViewController: UITableViewController, UISearchResultsUpdating {
     
     var songs = ["Jesus is my Lord", "Kartharai Thuthipom"]
+    var filteredTableData = [String]()
+    var resultSearchController = UISearchController()
     
     // MARK: - UIViewController lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.editing = true
+        self.resultSearchController = ({let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.sizeToFit()
+            self.tableView.tableHeaderView = controller.searchBar
+            return controller
+        })()
+        self.tableView.reloadData()
     }
     
     // MARK: - UITableViewDataSource
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return songs.count
+        if(self.resultSearchController.active){
+            return self.filteredTableData.count
+        }
+        else {
+            return self.songs.count
+
+        }
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("SongCell", forIndexPath: indexPath)
-        cell.textLabel?.text = songs[indexPath.row]
-        return cell
+        if(self.resultSearchController.active){
+            cell.textLabel?.text = filteredTableData[indexPath.row]
+            return cell
+        }
+        else{
+            cell.textLabel?.text = songs[indexPath.row]
+            return cell
+
+        }
     }
     
     override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
@@ -37,6 +60,17 @@ class SongsTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, shouldIndentWhileEditingRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         return false
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController)
+    {
+        filteredTableData.removeAll(keepCapacity: false)
+        
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (songs as NSArray).filteredArrayUsingPredicate(searchPredicate)
+        filteredTableData = array as! [String]
+        
+        self.tableView.reloadData()
     }
     
     /*override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
