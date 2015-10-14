f4dcc363c682bee93aa03df224010ee54c8c0d7f
Noah <nayagam1989@gmail.com>
2015-10-14 10:45:39 +0800

    #5 Modified the codes in existing features

--- WorshipSongs/WorshipSongs/SongTableViewController.swift
+++ WorshipSongs/WorshipSongs/SongTableViewController.swift
@@ -1,99 +1,85 @@
 //
-//  SongTableViewController.swift
-//  WorshipSongs
+//  FruitsTableViewController.swift
+//  TableExample
 //
-//  Created by Noah Nayagam on 10/12/15.
-//  Copyright © 2015 com.mcruncher. All rights reserved.
+//  Created by Ralf Ebert on 27/04/15.
+//  Copyright (c) 2015 Example. All rights reserved.
 //
 
 import UIKit
 
-class SongTableViewController: UITableViewController {
-
+class SongTableViewController: UITableViewController, UISearchResultsUpdating {
+    
+    var songs = ["Jesus is my Lord", "Kartharai Thuthipom"]
+    var filteredTableData = [String]()
+    var resultSearchController = UISearchController()
+    
+    // MARK: - UIViewController lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.tableView.editing = true
+        self.resultSearchController = ({let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.sizeToFit()
+            self.tableView.tableHeaderView = controller.searchBar
+            return controller
+        })()
+        self.tableView.reloadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 3
-    }
-
+    
+    // MARK: - UITableViewDataSource
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 5
+        if(self.resultSearchController.active){
+            return self.filteredTableData.count
+        }
+        else {
+            return self.songs.count
+            
+        }
     }
-
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("SongCell", forIndexPath: indexPath)
-
-        // Configure the cell...
-        cell.textLabel?.text = "Section \(indexPath.section) Row\(indexPath.row)"
-
-        return cell
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        if(self.resultSearchController.active){
+            cell.textLabel?.text = filteredTableData[indexPath.row]
+            return cell
+        }
+        else{
+            cell.textLabel?.text = songs[indexPath.row]
+            return cell
+            
+        }
     }
     
-
-    /*override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        return "Section \(section)"
+    override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        return .None
+    }
+    
+    override func tableView(tableView: UITableView, shouldIndentWhileEditingRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController)
+    {
+        filteredTableData.removeAll(keepCapacity: false)
+        
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (songs as NSArray).filteredArrayUsingPredicate(searchPredicate)
+        filteredTableData = array as! [String]
+        
+        self.tableView.reloadData()
+    }
+    
+    /*override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+    let movedObject = self.songs[sourceIndexPath.row]
+    songs.removeAtIndex(sourceIndexPath.row)
+    songs.insert(movedObject, atIndex: destinationIndexPath.row)
+    NSLog("%@", "\(sourceIndexPath.row) => \(destinationIndexPath.row) \(songs)")
+    // To check for correctness enable:  self.tableView.reloadData()
     }*/
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
---
+++ WorshipSongs/WorshipSongs/Songs.swift
@@ -0,0 +1,13 @@
+//
+//  Songs.swift
+//  WorshipSongs
+//
+//  Created by Noah Nayagam on 10/14/15.
+//  Copyright © 2015 com.mcruncher. All rights reserved.
+//
+
+import Foundation
+struct Song {
+    let title : String
+    let lyrics : String
+}
